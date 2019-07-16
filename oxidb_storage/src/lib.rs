994a1f813f21a2972afdcdd7348a43f302e75464
Daniel Ribeiro <daniel.ribeiro@resultadosdigitais.com.br>
2019-07-16 16:22:33 -0300

    Added docs to storage mod

--- oxidb_storage/src/lib.rs
+++ oxidb_storage/src/lib.rs
@@ -1,4 +1,20 @@
+#![deny(
+    missing_docs,
+    missing_debug_implementations,
+    missing_copy_implementations,
+    trivial_casts,
+    trivial_numeric_casts,
+    unsafe_code,
+    unused_import_braces
+)]
 #![feature(box_syntax)]
+
+//! # oxidb_core
+//!
+//! `oxidb_storage` is the storage abstraction layer for an `oxidb` database system implementation.
+//!
+//! ## Examples
+//!
 
 #[macro_use]
 extern crate bitflags;
@@ -6,19 +22,24 @@ extern crate failure;
 extern crate log;
 extern crate oxidb_core;
 
+/// `babylon` is `oxidb`'s default storage engine.
 pub mod babylon;
 
 use failure::Error;
 use oxidb_core::ColumnValueOps;
 use std::borrow::Cow;
 
+/// `StorageOps` define the basic interface of storage engines.
 pub trait StorageOps<'a> {
+    /// `ColumnValue` implementation of `ColumnValueOps`
     type ColumnValue: ColumnValueOps;
 
+    /// `iter` Returns an iterator over rows as arrays of `Self::ColumnValue` type items.
     fn iter<'b>(&'b self) -> Box<dyn Iterator<Item = Cow<'b, [Self::ColumnValue]>> + 'b>
     where
         [<Self as StorageOps<'a>>::ColumnValue]: std::borrow::ToOwned;
 
+    /// `insert_row` inserts a new row.
     fn insert_row<T>(&mut self, row: T) -> Result<(), Error>
     where
         T: ExactSizeIterator,

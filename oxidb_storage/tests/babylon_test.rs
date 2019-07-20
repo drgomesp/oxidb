ab9faddfc6042f4c638a37f574cb295524e5d692
Daniel Ribeiro <daniel.ribeiro@resultadosdigitais.com.br>
2019-07-20 00:42:33 -0300

    Added read test scenario

--- oxidb_storage/tests/babylon_test.rs
+++ oxidb_storage/tests/babylon_test.rs
@@ -1,9 +1,10 @@
+use oxidb_core::ColumnValue;
+
 #[cfg(test)]
 mod tests {
     use super::*;
-    use oxidb_core::types::ColumnValue;
-    use oxidb_storage::babylon::BabylonStorage;
-    use oxidb_storage::{StorageFactory, WriteOps};
+    use oxidb_core::ColumnValue;
+    use oxidb_storage::{BabylonStorage, ReadOps, StorageFactory, WriteOps};
 
     #[test]
     fn test_factory() {
@@ -11,24 +12,47 @@ mod tests {
     }
 
     #[test]
-    fn test_insert() {
+    fn test_write() {
         let mut storage: BabylonStorage = BabylonStorage::build().unwrap();
 
-        let cvs: Vec<_> = "1 foo bar"
-            .split(' ')
-            .map(|cv| match cv.parse::<u64>() {
-                Err(_) => match cv.parse::<i64>() {
-                    Err(_) => ColumnValue::StringLiteral(cv.into()),
-                    Ok(id) => ColumnValue::SignedInteger(-id),
-                },
-                Ok(id) => ColumnValue::UnsignedInteger(id),
-            })
-            .collect();
-
-        let rows: Vec<Vec<ColumnValue>> = vec![cvs.clone(); 3];
+        let row: Vec<_> = build_row("1 foo bar".to_string());
+        let rows: Vec<Vec<_>> = vec![row.clone(); 3];
 
         for r in &rows {
             assert!(storage.insert(r.into_iter().cloned()).is_ok());
         }
     }
+
+    #[test]
+    fn test_read() {
+        let mut storage: BabylonStorage = BabylonStorage::build().unwrap();
+
+        let row: Vec<_> = build_row("1 foo bar".to_string());
+        let rows: Vec<Vec<_>> = vec![row.clone(); 3];
+
+        for r in &rows {
+            assert!(storage.insert(r.into_iter().cloned()).is_ok());
+        }
+
+        let storage = storage;
+
+        for row in storage.iter() {
+            assert_eq!(row[0], ColumnValue::UnsignedInteger(1));
+            assert_eq!(row[1], ColumnValue::StringLiteral("foo".to_string()));
+            assert_eq!(row[2], ColumnValue::StringLiteral("bar".to_string()));
+        }
+    }
+}
+
+fn build_row(row_str: String) -> Vec<ColumnValue> {
+    row_str
+        .split(' ')
+        .map(|cv| match cv.parse::<u64>() {
+            Err(_) => match cv.parse::<i64>() {
+                Err(_) => ColumnValue::StringLiteral(cv.into()),
+                Ok(id) => ColumnValue::SignedInteger(-id),
+            },
+            Ok(id) => ColumnValue::UnsignedInteger(id),
+        })
+        .collect()
 }
